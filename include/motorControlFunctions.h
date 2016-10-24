234f81d404c618514700077845fe72a3b02fa04a
Nathaniel Peiffer <nathaniel@peiffer.com.au>
2016-10-24 21:44:50 +1100

    Changes to motorControlFunctions, begin header doc

--- include/motorControlFunctions.h
+++ include/motorControlFunctions.h
@@ -1,8 +1,13 @@
 #ifndef MOTORCONTROLFUNCTIONS_H_INCLUDED
 #define MOTORCONTROLFUNCTIONS_H_INCLUDED
 
+/**
+* Wait for a value to reach zero before allowing the thread to continue
+*
+* @param value Value to watch
+*/
 void waitForZero(int value);
-int cJoyThreshold(int i);
+float cJoyThreshold(int input);
 int setIntakeForks(const int dir);
 int toggleIntakeForks();
 int setLiftLeft(const int power);
@@ -11,12 +16,10 @@ int setDriveLeft(const int power);
 int setDriveRight(const int power);
 int setLift(const int power);
 int driveTime(const int l_power, const int r_power, const int timeMs);
+int driveStraightTime(const int power, const unsigned timeMs);
 int turnTime(const int power, const int timeMs);
 int turnGyro(const int power, const int deg);
 int driveQuad(const int power, const int ticks);
 int turnQuad(const int power, const int ticks);
-// int turnIME(const int power, const int ticks);
-// int driveIME(const int power, const int ticks);
-
 
 #endif //MOTORCONTROLFUNCTIONS_H_INCLUDED
