d0d91992b5a7d84340eebb8be4a6deeee7e28b99
Nathaniel Peiffer <nathaniel.peiffer@oceaninformatics.com>
2016-12-02 11:36:56 +1100

    re enable driver control and task management

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -1,6 +1,6 @@
 /**
  * @Date:   2016-10-22T14:32:24+11:00
-* @Last modified time: 2016-12-02T11:34:10+11:00
+* @Last modified time: 2016-12-02T11:36:24+11:00
  */
 
 #include "../include/main.h"
@@ -12,29 +12,16 @@ void operatorControl() {
 	  LIFT_SLEW_CONTROL_ENABLED = true;
 	  BASE_SLEW_CONTROL_ENABLED = true;
 
-	  //joyControlHandle = taskCreate(joystickControlTask, 1024, NULL, (TASK_PRIORITY_HIGHEST - 1));
-	  //solControlHandle = taskCreate(solenoidControlTask, TASK_DEFAULT_STACK_SIZE, NULL, TASK_PRIORITY_DEFAULT);
-	  //odoTaskHandle = taskCreate(odoUpdateTask, 1024, NULL, (TASK_PRIORITY_HIGHEST - 3));
+	  joyControlHandle = taskCreate(joystickControlTask, 1024, NULL, (TASK_PRIORITY_HIGHEST - 1));
+	  solControlHandle = taskCreate(solenoidControlTask, TASK_DEFAULT_STACK_SIZE, NULL, TASK_PRIORITY_DEFAULT);
+	  odoTaskHandle = taskCreate(odoUpdateTask, 1024, NULL, (TASK_PRIORITY_HIGHEST - 3));
 	  slewControlHandle = taskCreate(slewrateControl_task, 1024, NULL, (TASK_PRIORITY_HIGHEST - 1));
-	  //watchdogHandle = taskCreate(watchDogManagement, 64, NULL, TASK_PRIORITY_LOWEST + 1);
-
-	  lcdClear(LCD_PORT);
-	  lcdPrint(LCD_PORT, 1, "op init complete");
-	  // lcdClear(LCD_PORT);
-
-	  delay(1000);
-	  driveQuadPID(1000);
-	  // lcdClear(LCD_PORT);
+	  watchdogHandle = taskCreate(watchDogManagement, 64, NULL, TASK_PRIORITY_LOWEST + 1);
 
 	  while (true) {
-
-		    // if ((taskGetState(watchdogHandle) == TASK_DEAD || taskGetState(watchdogHandle) ==  TASK_RUNNABLE) && taskGetState(watchdogHandle) != TASK_RUNNING) {
-		    //     taskDelete(watchdogHandle);
-		    //     taskCreate(watchDogManagement, 64, NULL, TASK_PRIORITY_LOWEST + 1);
-		    // }
-
 		    lcdPrint(LCD_PORT, 1, "Main: %d.%03d V", powerLevelMain() / 1000, powerLevelMain()%1000);
 		    lcdPrint(LCD_PORT, 2, "Backup: %d.%03d V", powerLevelBackup() / 1000, powerLevelBackup()%1000);
+
 		    // lcdPrint(LCD_PORT, 1, "%d", encoderGet(enc_baseLeft));
 		    // lcdPrint(LCD_PORT, 2, "%d", encoderGet(enc_baseRight));
 		    // lcdPrint(LCD_PORT, 1, "%d", gyroGet(mainGyro));
